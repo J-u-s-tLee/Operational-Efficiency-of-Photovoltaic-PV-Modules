daff11a48205dc4ab5555a9573057c841dd887ac
leonorf20 <162609318+leonorf20@users.noreply.github.com>
2025-01-02 22:55:53 +0000

    Update model.py

--- model.py
+++ model.py
@@ -3,20 +3,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class SharedFeedForwardNN(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_classes):
+    def __init__(self, input_dim, hidden_dim, num_classes, dropout_rate):
         super(SharedFeedForwardNN, self).__init__()
         
         self.shared_layer_1 = nn.Linear(input_dim, hidden_dim)
         self.shared_layer_2 = nn.Linear(hidden_dim, hidden_dim)
+        self.shared_layer_3 = nn.Linear(hidden_dim, hidden_dim // 2)
+        self.dropout = nn.Dropout(p=dropout_rate)
         
-        self.regression_output = nn.Linear(hidden_dim, 1)
-    
-        self.classification_output = nn.Linear(hidden_dim, num_classes)
+        self.regression_output = nn.Linear(hidden_dim // 2, 1)
+        self.classification_output = nn.Linear(hidden_dim // 2, num_classes)
         
     def forward(self, x):
 
         x = torch.relu(self.shared_layer_1(x))
         x = torch.relu(self.shared_layer_2(x))
+        x = self.dropout(x)
+        x = torch.relu(self.shared_layer_3(x))
         
         reg_output = self.regression_output(x)
         
