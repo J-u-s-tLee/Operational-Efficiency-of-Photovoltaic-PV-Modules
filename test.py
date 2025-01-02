051c040cfa2f6c5feb4451989ac62c37f57309cf
leonorf20 <162609318+leonorf20@users.noreply.github.com>
2025-01-02 22:55:27 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 import torch
 import torch.nn.functional as F
-from sklearn.metrics import roc_auc_score, roc_curve, confusion_matrix, f1_score, precision_score, recall_score
+from sklearn.metrics import roc_auc_score, roc_curve, confusion_matrix, f1_score, precision_score, recall_score, r2_score, balanced_accuracy_score
+from model import SharedFeedForwardNN
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import json
 import os
 
-def test_model(model, test_loader, num_classes, save_dir):
+
+def test_model(model, test_loader, num_classes, save_dir, load_dir):
+    model_path = os.path.join(load_dir, "best_model.pth")
+
+    model.load_state_dict(torch.load(model_path, weights_only=True))
 
     model.eval() 
 
@@ -15,8 +20,10 @@ def test_model(model, test_loader, num_classes, save_dir):
     total_samples = 0 
     total_correct = 0 
 
-    all_labels = []
-    all_predictions = []
+    all_class_labels = []
+    all_reg_labels = []
+    all_class_predictions = []
+    all_reg_predictions = []
     all_probabilities = []
 
     with torch.no_grad():
@@ -26,33 +33,42 @@ def test_model(model, test_loader, num_classes, save_dir):
 
             labels = labels.to(torch.int64)
 
+            # Regression
             reg_loss = F.mse_loss(reg_output.squeeze(), labels[:, 0])
             running_reg_loss += reg_loss.item()
 
+            all_reg_labels.extend(labels[:, 0].cpu().numpy())
+            all_reg_predictions.extend(reg_output.squeeze().cpu().numpy())
+
+            # Classification
             probas = F.softmax (logits, dim=1)
             _, predicted_classes = torch.max(probas, 1)
             total_samples += labels[:, 1].size(0)
             total_correct += (predicted_classes == labels[:,1]).sum().item()
 
-            all_labels.extend(labels[:,1].cpu().numpy()) # GT labels
-            all_predictions.extend(predicted_classes.cpu().numpy()) 
+            all_class_labels.extend(labels[:,1].cpu().numpy()) # GT labels
+            all_class_predictions.extend(predicted_classes.cpu().numpy()) 
             all_probabilities.extend(probas.cpu().numpy())
-            
+
+    r2 = r2_score(all_reg_labels, all_reg_labels)
     avg_reg_loss = running_reg_loss / len(test_loader)
         
-    test_accuracy = total_correct / total_samples
-    f1 = f1_score(all_labels, all_predictions, average="weighted")
-    precision = precision_score(all_labels, all_predictions, average="weighted")
-    recall = recall_score(all_labels, all_predictions, average="weighted")
+    test_acc = total_correct / total_samples
+    balanced_test_acc = balanced_accuracy_score(all_class_labels, all_class_predictions)
+    f1 = f1_score(all_class_labels, all_class_predictions, average="weighted")
+    precision = precision_score(all_class_labels, all_class_predictions, average="weighted")
+    recall = recall_score(all_class_labels, all_class_predictions, average="weighted")
 
     print(f"\nRegression Loss (test): {avg_reg_loss:.4f}")
-    print(f"Test acc: {test_accuracy * 100:.2f}%")
+    print(f"R² Score: {r2:.4f}")
+    print(f"Test acc: {test_acc * 100:.2f}%")
+    print(f"Balanced Test acc: {balanced_test_acc * 100:.2f}%")
     print(f"F1-Score: {f1 * 100:.2f}%")
     print(f"Precision: {precision * 100:.2f}%")
     print(f"Recall: {recall *  100:.2f}%")
 
 
-    all_labels_onehot = np.eye(num_classes)[all_labels]
+    all_labels_onehot = np.eye(num_classes)[all_class_labels]
 
     auc_roc = {}
     plt.figure(figsize=(8, 6))
@@ -72,7 +88,7 @@ def test_model(model, test_loader, num_classes, save_dir):
 
     plt.savefig(f"{save_dir}/roc_curve.png")
 
-    conf_matrix = confusion_matrix(all_labels, all_predictions)
+    conf_matrix = confusion_matrix(all_class_labels, all_class_predictions)
     conf_matrix = conf_matrix.astype('float') / conf_matrix.sum(axis=1)[:, np.newaxis]
 
     plt.figure(figsize=(8, 6))
@@ -86,11 +102,14 @@ def test_model(model, test_loader, num_classes, save_dir):
 
 
     metrics = {
-        "Test - Accuracy": f"{test_accuracy:.4f}",
-        "Test - F1-Score": f"{f1:.4f}",
-        "Test - Precision": f"{precision:.4f}",
-        "Test - Recall": f"{recall:.4f}",
-        "Test - AUC-ROC": {key: f"{value:.4f}" for key, value in auc_roc.items()},
+        "Test, Reg - R^2 Score": f"{r2:.4f}",
+        "Test, Reg - MSE": f"{avg_reg_loss:.4f}",
+        "Test, Class - Accuracy": f"{test_acc:.4f}",
+        "Test, Class - Balanced Accuracy": f"{balanced_test_acc:.4f}",
+        "Test, Class - F1-Score": f"{f1:.4f}",
+        "Test, Class - Precision": f"{precision:.4f}",
+        "Test, Class - Recall": f"{recall:.4f}",
+        "Test, Class - AUC-ROC": {key: f"{value:.4f}" for key, value in auc_roc.items()},
     }
 
     metrics_file = os.path.join(save_dir, "metrics.json")
