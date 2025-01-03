c3fd53bc3b95fd622413389b6207dada849a1d71
Leandro Ribeiro <154263742+J-u-s-tLee@users.noreply.github.com>
2025-01-03 19:34:53 +0000

    Update PreProcessedData.py

--- PreProcessedData.py
+++ PreProcessedData.py
@@ -11,24 +11,17 @@ def split_labels(data):
     efficiency_level = data[["efficiency_level"]]
     return expected_efficiency, efficiency_level
 
-def split_data(data, column_1, column_2, test_size=0.3, random_state=42, model="Linear Regression"):
+def split_data(data, column_1, column_2, test_size=0.3, random_state=42):
     X = data.drop(columns=[column_1, column_2])
     y = data[[column_1, column_2]]
 
-    if model == "Linear Regression": 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
-        y_train_1, _ = split_labels(y_train)
-        y_test_1, _ = split_labels(y_test)
-        
-        return X_train, X_test, y_train_1, y_test_1
-    else: 
-        X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=test_size, random_state=random_state)
-        X_validation, X_test, y_validation, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=random_state)
-        y_train_1, y_train_2 = split_labels(y_train)
-        y_validation_1, y_validation_2 = split_labels(y_validation)
-        y_test_1, y_test_2 = split_labels(y_test)
+    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    X_validation, X_test, y_validation, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=random_state)
+    y_train_1, y_train_2 = split_labels(y_train)
+    y_validation_1, y_validation_2 = split_labels(y_validation)
+    y_test_1, y_test_2 = split_labels(y_test)
 
-        return X_train, X_validation, X_test, y_train_1, y_validation_1, y_test_1,  y_train_2, y_validation_2, y_test_2
+    return X_train, X_validation, X_test, y_train_1, y_validation_1, y_test_1,  y_train_2, y_validation_2, y_test_2
 
 def encode_categorical(X_train, X_validation, X_test, categorical_cols):
     encoder = OneHotEncoder(sparse_output=False)
